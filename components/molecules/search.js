09d645a9ee4c7a2f4e9e641ddf4a960f3b96d6ca
Budi Setyo <budisetyo176@gmail.com>
2022-06-22 00:44:12 +0700

    fix: components search

--- components/molecules/search.js
+++ components/molecules/search.js
@@ -5,7 +5,9 @@ const Search = ({ onChange, onKeyDown }) => {
   return (
     <>
       <InputGroup className="bg-gray-100 rounded-md">
-        <InputLeftElement children={<SearchIcon color="#0000007A" />} />
+        <InputLeftElement>
+          <SearchIcon color="#0000007A" />
+        </InputLeftElement>
         <Input
           onChange={onChange}
           onKeyDown={onKeyDown}
